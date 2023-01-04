9fcc4b498b49dd0df09e12a033b9d10160bb2aff
SNQ-2001 <taishin05109@gmail.com>
2023-01-04 23:37:19 +0900

    add: プロジェクトを作成

---
+++ AppleSystemStatusMenuBar/Application/AppleSystemStatusMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSystemStatusMenuBarApp.swift
+//  AppleSystemStatusMenuBar
+//
+//  Created by 宮本大新 on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct AppleSystemStatusMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSystemStatusMenuBar/Model/Entity/AppleSystemStatus.swift
@@ -0,0 +1,55 @@
+//
+//  AppleSystemStatus.swift
+//  AppleSystemStatusMenuBar
+//
+//  Created by 宮本大新 on 2023/01/04.
+//
+
+import Foundation
+
+// MARK: - AppleSystemStatus
+struct AppleSystemStatus: Codable {
+    let services: [Service]
+}
+
+// MARK: - Service
+struct Service: Codable {
+    let redirectURL: String
+    let events: [Event]
+    let serviceName: String
+
+    enum CodingKeys: String, CodingKey {
+        case redirectURL = "redirectUrl"
+        case events, serviceName
+    }
+}
+
+// MARK: - Event
+struct Event: Codable {
+    let usersAffected: String
+    let epochStartDate, epochEndDate: Int
+    let messageID: String
+    let statusType: StatusType
+    let datePosted: Date
+    let startDate: Date
+    let endDate: Date
+    let affectedServices: [String]?
+    let eventStatus: EventStatus
+    let message: String
+
+    enum CodingKeys: String, CodingKey {
+        case usersAffected, epochStartDate, epochEndDate
+        case messageID = "messageId"
+        case statusType, datePosted, startDate, endDate, affectedServices, eventStatus, message
+    }
+}
+
+enum EventStatus: String, Codable {
+    case resolved = "resolved"
+    case completed = "completed"
+}
+
+enum StatusType: String, Codable {
+    case issue = "Issue"
+    case outage = "Outage"
+}
