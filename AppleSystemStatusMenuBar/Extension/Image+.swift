4c119181ea6d419d3df2302fa54c4ac94d418047
SNQ-2001 <taishin05109@gmail.com>
2023-01-04 23:44:02 +0900

    add: Image拡張を作成

---
+++ AppleSystemStatusMenuBar/Extension/Image+.swift
@@ -0,0 +1,14 @@
+//
+//  Image+.swift
+//  AppleSystemStatusMenuBar
+//
+//  Created by 宮本大新 on 2023/01/04.
+//
+
+import SwiftUI
+
+extension Image {
+    static let circle = Image(systemName: "circle.fill")
+    static let triangle = Image(systemName: "triangle.fill")
+    static let diamond = Image(systemName: "diamond.fill")
+}
