7b2e6acefbdf26358e7a892580af9792af038bb7
SNQ-2001 <taishin05109@gmail.com>
2023-01-23 17:35:13 +0900

    feat: fetchAppleDeveloperSystemStatusを実装

--- AppleSystemStatusMenuBar/Model/Repository/AppleRepository.swift
+++ AppleSystemStatusMenuBar/Model/Repository/AppleRepository.swift
@@ -10,6 +10,7 @@ import Foundation
 
 protocol AppleRepository {
     func fetchAppleSystemStatus(localeName: String) -> AnyPublisher<AppleSystemStatus, Error>
+    func fetchAppleDeveloperSystemStatus() -> AnyPublisher<AppleDeveloperSystemStatus, Error>
 }
 
 class AppleRepositoryImpl: AppleRepository {
@@ -22,17 +23,40 @@ class AppleRepositoryImpl: AppleRepository {
         guard let url = endpoint.url else { return Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
         return APIClient.fetch(url: url)
     }
+
+    func fetchAppleDeveloperSystemStatus() -> AnyPublisher<AppleDeveloperSystemStatus, Error> {
+        let endpoint = Endpoint(
+            host: "www.apple.com",
+            path: "/support/systemstatus/data/system_status_en_US.js",
+            queryItems: []
+        )
+        guard let url = endpoint.url else { return Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
+        return APIClient.fetch(url: url)
+    }
 }
 
 class AppleRepositoryMock: AppleRepository {
     func fetchAppleSystemStatus(localeName _: String) -> AnyPublisher<AppleSystemStatus, Error> {
         Future { promise in
             promise(.success(.init(services: [
-                .init(redirectUrl: nil, events: [], serviceName: "Mock-1"),
-                .init(redirectUrl: nil, events: [], serviceName: "Mock-2"),
-                .init(redirectUrl: nil, events: [], serviceName: "Mock-3"),
-                .init(redirectUrl: nil, events: [], serviceName: "Mock-4"),
-                .init(redirectUrl: nil, events: [], serviceName: "Mock-5")
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-1-AppleSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-2-AppleSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-3-AppleSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-4-AppleSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-5-AppleSystemStatus")
+            ])))
+        }
+        .eraseToAnyPublisher()
+    }
+
+    func fetchAppleDeveloperSystemStatus() -> AnyPublisher<AppleDeveloperSystemStatus, Error> {
+        Future { promise in
+            promise(.success(.init(services: [
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-1-AppleDeveloperSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-2-AppleDeveloperSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-3-AppleDeveloperSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-4-AppleDeveloperSystemStatus"),
+                .init(redirectUrl: nil, events: [], serviceName: "Mock-5-AppleDeveloperSystemStatus")
             ])))
         }
         .eraseToAnyPublisher()
